39a6349fb9101cf118f389c1966135fa7a7f9d14
agamya1612 <46354525+agamya1612@users.noreply.github.com>
2020-08-05 17:10:36 +0530

    Add files via upload

---
+++ oddeve.java
@@ -0,0 +1,15 @@
+
+public class oddeve {
+	public static void main(String args[])
+	{
+		int n=10;
+		if(n%2==0)
+		{
+			System.out.println("Even No "+ n);
+			
+		}
+		else
+			System.out.println("Odd No "+ n);
+	}
+
+}
