9095bae375972f583772b72f4bae0e86da6500c0
agamya1612 <46354525+agamya1612@users.noreply.github.com>
2020-08-13 16:52:18 +0530

    Add files via upload

---
+++ task6.java
@@ -0,0 +1,19 @@
+package com.mypack.countries;
+
+public class task6 {
+	
+	public static void main(String[] args) {
+		India i=new India();
+		Brazil b=new Brazil();
+		Norway n=new Norway();
+		Qatar q=new Qatar();
+		Turkey k= new Turkey();
+		System.out.println(i.phnumberformat("+91-991760479a"));
+		System.out.println(b.phnumberformat("+55 15 99999-9"));
+		System.out.println(n.phnumberformat("+47-49-99-99-99"));
+		System.out.println(q.phnumberformat("974 33999999"));
+		System.out.println(k.phnumberformat("+90 509-999999"));
+		
+	}
+
+}
